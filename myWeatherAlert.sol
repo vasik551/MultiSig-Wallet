86fefb2b9d512f8fd51e6e277fec635e8e4a5cfd
vasik551 <vkazakov551@gmail.com>
2023-09-11 15:10:33 +0100

    Set Alert Location and Warning

--- myWeatherAlert.sol
+++ myWeatherAlert.sol
@@ -18,28 +18,6 @@ contract WeatherWarning is Ownable {
     // Event to log when a warning is set
     event WarningSet(string location, string warning, address indexed setter);
 
-    // Array of locations and warning messages
-    string[] public locations;
-    string[] public warningMessages;
-
-    // Constructor to initialize locations and warning messages
-    constructor() {
-        locations = ["City A", "City B", "City C"];
-        warningMessages = ["Heavy Rain Expected", "High Wind Alert", "Flash Flood Warning"];
-    }
-
-    // Function to generate a random warning message and set it automatically
-    function generateWarning() public onlyOwner {
-        uint256 randomLocationIndex = uint256(keccak256(abi.encodePacked(block.timestamp))) % locations.length;
-        uint256 randomWarningIndex = uint256(keccak256(abi.encodePacked(block.difficulty))) % warningMessages.length;
-
-        string memory location = locations[randomLocationIndex];
-        string memory warning = warningMessages[randomWarningIndex];
-
-        // Automatically set the generated warning
-        setWarning(location, warning);
-    }
-
     // Function to set a warning for a specific location
     function setWarning(string memory location, string memory warning) public onlyOwner {
         warnings[location] = warning;
@@ -51,4 +29,3 @@ contract WeatherWarning is Ownable {
         return warnings[location];
     }
 }
-
